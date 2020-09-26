7a961974c4ea42dcc99e745a5428286f238b90ef
Romén Rodríguez-Gil <rrodriguezgil@cloudbees.com>
2020-09-26 12:09:29 +0100

    Second test case. Now failing.

--- test-letra-dni.py
+++ test-letra-dni.py
@@ -5,6 +5,8 @@ from letra_dni import letra_dni
 class TestLetraDNI(unittest.TestCase):
     def test_12345678(self):
         self.assertEqual(letra_dni("12345678"), "Z")
+    def test_87654321(self):
+        self.assertEqual(letra_dni("87654321"), "X")
 
 if __name__ == '__main__':
     unittest.main()
