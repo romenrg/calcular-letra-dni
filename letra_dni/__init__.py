b82b0f9222ecaa091ef0d834cbe1303ae6fdd378
Romén Rodríguez-Gil <rrodriguezgil@cloudbees.com>
2020-12-20 11:48:36 +0000

    Moved logger to class attribute
    To make sure it works when used as a module from another program

--- letra_dni/__init__.py
+++ letra_dni/__init__.py
@@ -30,13 +30,13 @@ class LetraDni:
     }
     def __init__(self, digits):
         self.digits = int(digits)
+        logging.config.fileConfig('letra_dni/logging.conf')
+        self.logger = logging.getLogger('calcularLetraDNILogger')
     def calculate(self):
         mod = self.digits % len(self.mod_to_letter)
-        logger.debug('Mod is: '+ str(mod))
+        self.logger.debug('Mod is: '+ str(mod))
         return self.mod_to_letter[str(mod)]
 
 if __name__ == '__main__':
-    logging.config.fileConfig('letra_dni/logging.conf')
-    logger = logging.getLogger('calcularLetraDNILogger')
     myDni = LetraDni(sys.argv[1])
     print(myDni.calculate())
